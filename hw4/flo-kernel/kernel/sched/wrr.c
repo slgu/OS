9c54dd9aff4851380be49ea8ed00dbdb81c54954
slgu <sg3301@columbia.edu>
2015-11-03 21:05:18 -0500

    partially done for wrr, need test and load balance

--- hw4/flo-kernel/kernel/sched/wrr.c
+++ hw4/flo-kernel/kernel/sched/wrr.c
@@ -1,5 +1,4 @@
 #include "sched.h"
-
 #define FOREGROUND_TYPE 0
 #define BACKGROUND_TYPE 1
 #define FOREGROUND_WEIGHT 10
@@ -14,11 +13,21 @@ static int get_task_group(struct task_struct * p)
 		return BACKGROUND_TYPE;
 }
 
+static void 
+__enqueue_entity_wrr(struct sched_wrr_entity *wrr_se, struct rq *rq, int head)
+{
+	/* add entity to queue */
+	if (head)
+		list_add(&wrr_se->run_list, &rq->wrr.wrr_entity_list);
+	else
+		list_add_tail(&wrr_se->run_list, &rq->wrr.wrr_entity_list);
+
+}
 static void
 enqueue_task_wrr(struct rq *rq, struct task_struct *p, int flags)
 {
 	/* add new task the tail */
-	struct sched_wrr_entity * wrr_se = &p.wrr;
+	struct sched_wrr_entity * wrr_se = &p->wrr;
 	int task_type = get_task_group(p);
 	if (flags & ENQUEUE_WAKEUP)
 		wrr_se->timeout = 0;
@@ -33,36 +42,42 @@ enqueue_task_wrr(struct rq *rq, struct task_struct *p, int flags)
 	/* increase load */
 }
 
-static void 
-__enqueue_entity_wrr(struct sched_wrr_entity *wrr_se, struct rq *rq, int head)
+
+static void
+__dequeue_entity_wrr(struct sched_wrr_entity *wrr_se, struct rq *rq)
 {
-	/* add entity to queue */
-	if (head)
-		list_add(&wrr_se.run_list, &rq->wrr.wrr_entity_list);
-	else
-		list_add_tail(&wrr_se.run_list, &rq->wrr.wrr_entity_list);
-
+	list_del(&wrr_se->run_list);
 }
-
 static void
 dequeue_task_wrr(struct rq *rq, struct task_struct *p, int flags)
 {
+	int task_type = get_task_group(p);
 	/* delete a item from the wrr_rq */
 	__dequeue_entity_wrr(&p->wrr, rq);
-	int task_type = get_task_group(p);
 	if (task_type == FOREGROUND_TYPE)
 		rq->nr_running = rq->nr_running - FOREGROUND_WEIGHT;
 	else
 		rq->nr_running = rq->nr_running - BACKGROUND_WEIGHT;
 }
 
-static void
-__dequeue_entity_wrr(struct sched_wrr_entity *wrr_se, struct rq *rq)
+
+
+static void update_curr_wrr(struct rq * rq)
 {
-	list_del(&wrr_se.run_list, &rq->wrr.wrr_entity_list);
+	struct task_struct *curr = rq->curr;
+	u64 delta_exec;
+	if (curr->sched_class != &wrr_sched_class)
+		return;
+	delta_exec = rq->clock_task - curr->se.exec_start;
+	if (unlikely((s64)delta_exec <= 0))
+		return;
+	schedstat_set(curr->se.statistics.exec_max,
+			max(curr->se.statistics.exec_max, delta_exec));
+	curr->se.sum_exec_runtime += delta_exec;
+	account_group_exec_runtime(curr, delta_exec);
+	curr->se.exec_start = rq->clock_task;
+	cpuacct_charge(curr, delta_exec);
 }
-
-
 
 static void put_prev_task_wrr(struct rq *rq, struct task_struct *prev)
 {
@@ -72,45 +87,32 @@ static void put_prev_task_wrr(struct rq *rq, struct task_struct *prev)
 
 static void watchdog(struct rq *rq, struct task_struct *p)
 {
-
+	/* need implement */
+	return;
 }
 static void task_tick_wrr(struct rq *rq, struct task_struct *p, int queued)
 {
-	struct sched_wrr_entity * wrr_se = &p->wrr;
-	update_curr_wrr(rq);
 	int task_type = get_task_group(p);
+	update_curr_wrr(rq);
+	watchdog(rq, p);
 	if (p->policy != SCHED_WRR)
 		return;
 	if (--p->wrr.time_slice)
 		return;
 	if (task_type == FOREGROUND_TYPE)
-		p->wrr.time_slice = 100 * FOREGROUND_WEIGHT;
+		p->wrr.time_slice = RR_TIMESLICE * FOREGROUND_WEIGHT;
 	else
-		p->wrr.time_slice = 100 * BACKGROUND_WEIGHT;
-	/* move to the tail */
-	list_move_tail(&p->wrr_se.run_list, &rq->wrr.wrr_entity_list);
-	/* reschedule */
-	resched_curr(rq);
+		p->wrr.time_slice = RR_TIMESLICE * BACKGROUND_WEIGHT;
+	if (p->wrr.run_list.prev != p->wrr.run_list.next) {
+		/* move to the tail */
+		list_move_tail(&p->wrr.run_list, &rq->wrr.wrr_entity_list);
+		/* reschedule */
+		set_tsk_need_resched(p);
+	}
 }
 
 static void set_curr_task_wrr(struct rq *rq)
 {
-}
-static void update_curr_wrr(struct rq * rq)
-{
-	struct task_struct *curr = rq->curr;
-	u64 delta_exec;
-	if (curr->sched_class != &wrr_sched_class)
-		return;
-	delta_exec = rq_clock_task(rq) - curr->se.exec_start;
-	if (unlikely((s64)delta_exec <= 0))
-		return;
-	schedstat_set(curr->se.statistics.exec_max,
-			max(curr->se.statistics.exec_max, delta_exec));
-	curr->se.sum_exec_runtime += delta_exec;
-	account_group_exec_runtime(curr, delta_exec);
-	curr->se.exec_start = rq_clock_task(rq);
-	cpuacct_charge(curr, delta_exec);
 }
 static void switched_to_wrr(struct rq *rq, struct task_struct *p)
 {
@@ -123,27 +125,21 @@ prio_changed_wrr(struct rq *rq, struct task_struct *p, int oldprio)
 	return;
 }
 
-static unsigned int get_rr_interval_idle(struct rq *rq, struct task_struct *task)
-{
-	return 0;
-}
 
-static check_preempt_curr_wrr(struct rq *rq, struct task_struct *p, int flags)
+static void check_preempt_curr_wrr(struct rq *rq, struct task_struct *p, int flags)
 {
 	/* if preempt is needed */
 	return;
 }
 static struct task_struct *
-pick_next_task_wrr(struct rq *rq, struct task_struct *prev)
+pick_next_task_wrr(struct rq *rq)
 {
-	/* set prev task to end of sche class queue */
-	put_prev_task(rq, prev);
+	struct sched_wrr_entity * entity = list_first_entry(&rq->wrr.wrr_entity_list, struct sched_wrr_entity, run_list); 
+	struct task_struct * p = list_entry(entity, struct task_struct, wrr);
 	if (list_empty(&rq->wrr.wrr_entity_list)) {
 		return NULL;
 	}
-	struct sched_wrr_entity * entity = list_first_entry(&rq->wrr.wrr_entity_list, struct sched_wrr_entity *, run_list); 
-	struct task_struct * p = list_entry(entity, struct task_struct *, wrr_se);
-	p->se.exec_start = rq_clock_task(rq);
+	p->se.exec_start = rq->clock_task;
 	/* remove from wrr queue */
 	dequeue_task_wrr(rq, p, 0);
 	return p;
@@ -168,31 +164,35 @@ static int find_lowest_cpu(void)
 }
 
 static int
-select_task_rq_wrr(struct task_struct *p, int cpu, int sd_flag, int flags)
+select_task_rq_wrr(struct task_struct *p, int sd_flag, int flags)
 {
-	struct task_struct *curr;
-	struct rq *rq;
 	int target;
+	int cpu;
+	cpu = task_cpu(p);
+	if (p->wrr.nr_cpus_allowed == 1)
+		return cpu;
 	/* For anything but wake ups, just return the task_cpu */
 	if (sd_flag != SD_BALANCE_WAKE && sd_flag != SD_BALANCE_FORK)
 		return cpu;
 	/* lock needed */
 	rcu_read_lock();
-	int target = find_lowest_cpu();	
+	target = find_lowest_cpu();	
 	rcu_read_unlock();
 	if (target == -1)
 		return cpu;
 	else
 		return target;
 }
-
+static unsigned int get_rr_interval_wrr(struct rq *rq, struct task_struct *task)
+{
+	return RR_TIMESLICE;
+}
 const struct sched_class wrr_sched_class = {
 	.next			= &idle_sched_class, 
 	.enqueue_task		= enqueue_task_wrr,
 	.dequeue_task		= dequeue_task_wrr,
 
 	.check_preempt_curr	= check_preempt_curr_wrr,
-
 	.pick_next_task		= pick_next_task_wrr,
 	.put_prev_task		= put_prev_task_wrr,
 
@@ -209,5 +209,4 @@ const struct sched_class wrr_sched_class = {
 
 	.prio_changed		= prio_changed_wrr,
 	.switched_to		= switched_to_wrr,
-	.update_curr		= update_curr_wrr,
 };
