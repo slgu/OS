3a7fcde365a68cf9f8383eed2befe4e7cf0dd300
slgu <sg3301@columbia.edu>
2015-11-22 21:00:25 -0500

    test done

--- hw5/test/test.c
+++ hw5/test/test.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/time.h>
+#include <sys/wait.h>
 #include <sys/mman.h>
 #include <sys/resource.h>
 #include <signal.h>
@@ -13,9 +14,11 @@
 #include <sys/syscall.h>
 #include <errno.h>
 #include "err.h"
-
+#define NUM 10
+#define BIG_NUM 2000
+#define PAGE_SIZE 4096 
+#define SUPER_BIG_NUM 20000
 static int print_maps(void);
-
 static void pr_errno(const char *msg)
 {
 	printf("error: %s, %s", strerror(errno), msg);
@@ -28,36 +31,81 @@ static void pr_err(const char *msg)
 
 static int test_1(void)
 {
+	char * p = (char *)mmap(0, NUM * PAGE_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);	
+	if (0)
+		p[0] = '0';
 	return 0;
 }
 
 static int test_2(void)
 {
+	char * p = (char *)mmap(0, NUM * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);	
+	if (0)
+		p[0] = '0';
+	int i = 0;
+	char a;
+	for (i = 0; i < NUM; ++i)
+		a = p[i * PAGE_SIZE];
+	if (0)
+		printf("%c", a);
 	return 0;
 }
 
 static int test_3(void)
 {
+	char * p = (char *)mmap(0, NUM * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);	
+	if (0)
+		p[0] = '0';
+	int i = 0;
+	char a;
+	for (i = 1; i < NUM; i += 2)
+		a = p[i * PAGE_SIZE];
+	if (0)
+		printf("%c", a);
 	return 0;
 }
 
 static int test_4(void)
 {
+	char * p = (char *)mmap(0, NUM * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);	
+	if (0)
+		p[0] = '0';
+	int i;
+	for (i = 0; i < NUM / 2; ++i)
+		p[i * PAGE_SIZE] = 0;
 	return 0;
 }
 
 static int test_5(void)
 {
+	char * p = (char *)mmap(0, NUM * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);	
+	if (0)
+		p[0] = '0';
+	int i;
+	char a;
+
+	for (i = 0; i < 4; ++i)
+		a = p[i * PAGE_SIZE];
+	for (i = 6; i < NUM; ++i)
+		p[i * PAGE_SIZE] = 0;
+	if (0)
+		printf("%c",a);
 	return 0;
 }
 
 static int test_6(void)
 {
+	char * p = (char *)mmap(0, BIG_NUM * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);	
+	if (0)
+		p[0] = '0';
 	return 0;
 }
 
 static int test_7(void)
 {
+	char * p = (char *)mmap(0, SUPER_BIG_NUM * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);	
+	if (0)
+		p[0] = 0;
 	return 0;
 }
 
@@ -66,14 +114,16 @@ int main(int argc, char **argv)
 	/*
 	 * Change this main function as you see fit.
 	 */
-	test_1();
-	print_maps();
-	test_2();
-	test_3();
-	test_4();
-	test_5();
-	test_6();
+	if (0) {
+		test_1();
+		test_2();
+		test_3();
+		test_4();
+		test_5();
+		test_6();
+	}
 	test_7();
+	print_maps();
 	return 0;
 }
 
